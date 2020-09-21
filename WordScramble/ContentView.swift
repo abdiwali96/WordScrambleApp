1c19db11851da242bf4e82f17f2aff8e8fed19e8
abdiwali96 <abdiwali96@gmail.com>
2020-09-21 22:39:34 +0100

    file load in

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,28 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let people = ["Finn","abdiliwa", "lol"]
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+   
+
+    
     var body: some View {
-        
-        NavigationView{
-            List {
-                Section(header: Text("Section1")){
-                    ForEach(people, id: \.self) {
-                            Text($0)
-                        }
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
                 }
-                Section (header: Text("Section2")){
-                    ForEach(0 ..< 5){ num in
-                    Text("rows")
-                    }
-                }
-                Section(header: Text("Section3")){
-                    Text("row 3")
-                    Text("row 3")
-                }
-            }.listStyle(GroupedListStyle())
+            }
+            .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
         }
     }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+
+        // extra validation to come
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    //fatal error code
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+
+                // If we are here everything has worked, so we can exit
+                return
+            }
+        }
+
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
